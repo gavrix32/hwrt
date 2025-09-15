2d517381885cd797754963ffd08e2080890b9390
gavrix32 <superdimoxa228@gmail.com>
2025-09-16 01:11:03 +0300

    Added physical device, queue family selection and logging

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS 1
 #include <vulkan/vulkan_raii.hpp>
+#include <spdlog/spdlog.h>
 
 #define GLFW_INCLUDE_VULKAN
 #include <set>
@@ -20,11 +21,25 @@ constexpr bool enable_validation_layers = false;
 constexpr bool enable_validation_layers = true;
 #endif
 
+static spdlog::level::level_enum to_spdlog_level(const vk::DebugUtilsMessageSeverityFlagBitsEXT severity) {
+    if (severity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eError) return spdlog::level::err;
+    if (severity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning) return spdlog::level::warn;
+    if (severity & vk::DebugUtilsMessageSeverityFlagBitsEXT::eInfo) return spdlog::level::info;
+    return spdlog::level::debug;
+}
+
+static std::string_view message_type_to_string(vk::DebugUtilsMessageTypeFlagsEXT type) {
+    if (type & vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation) return "VALIDATION";
+    if (type & vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance) return "PERFORMANCE";
+    if (type & vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral) return "GENERAL";
+    return "UNKNOWN";
+}
+
 static VKAPI_ATTR vk::Bool32 VKAPI_CALL debug_callback(vk::DebugUtilsMessageSeverityFlagBitsEXT severity,
-                                                       const vk::DebugUtilsMessageTypeFlagsEXT type,
+                                                       vk::DebugUtilsMessageTypeFlagsEXT type,
                                                        const vk::DebugUtilsMessengerCallbackDataEXT *p_callback_data,
                                                        void *) {
-    std::cerr << "validation layer: type " << to_string(type) << " msg: " << p_callback_data->pMessage << std::endl;
+    spdlog::log(to_spdlog_level(severity), "[Vulkan {}] {}", message_type_to_string(type), p_callback_data->pMessage);
 
     return vk::False;
 }
@@ -40,6 +55,12 @@ std::string_view get_physical_device_type_name(vk::PhysicalDeviceType type) {
 }
 
 int main() {
+#ifdef NDEBUG
+    spdlog::set_level(spdlog::level::warn);
+#else
+    spdlog::set_level(spdlog::level::trace);
+#endif
+
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
@@ -105,12 +126,16 @@ int main() {
         .ppEnabledExtensionNames = required_extensions.data(),
     };
 
-    vk::DebugUtilsMessengerCreateInfoEXT debug_utils_messenger_create_info_EXT;
-
     if (enable_validation_layers) {
         instance_create_info.enabledLayerCount = static_cast<uint32_t>(required_layers.size());
         instance_create_info.ppEnabledLayerNames = required_layers.data();
+    }
 
+    const auto instance = vk::raii::Instance(context, instance_create_info);
+
+    vk::raii::DebugUtilsMessengerEXT debug_messenger{nullptr};
+
+    if (enable_validation_layers) {
         vk::DebugUtilsMessageSeverityFlagsEXT severity_flags(
             vk::DebugUtilsMessageSeverityFlagBitsEXT::eVerbose | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning |
             vk::DebugUtilsMessageSeverityFlagBitsEXT::eError);
@@ -119,38 +144,35 @@ int main() {
             vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance |
             vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation);
 
-        debug_utils_messenger_create_info_EXT = vk::DebugUtilsMessengerCreateInfoEXT{
+        auto debug_utils_messenger_create_info_EXT = vk::DebugUtilsMessengerCreateInfoEXT{
             .messageSeverity = severity_flags,
             .messageType = message_type_flags,
             .pfnUserCallback = &debug_callback
         };
-    }
 
-    const auto instance = vk::raii::Instance(context, instance_create_info);
-
-    if (enable_validation_layers) {
-        vk::raii::DebugUtilsMessengerEXT debug_messenger = instance.createDebugUtilsMessengerEXT(
+        debug_messenger = instance.createDebugUtilsMessengerEXT(
             debug_utils_messenger_create_info_EXT);
     }
 
     std::set<std::string> required_device_extension_names;
+    required_device_extension_names.insert(vk::KHRDynamicRenderingExtensionName);
     required_device_extension_names.insert(vk::KHRRayTracingPipelineExtensionName);
-    required_device_extension_names.insert(vk::NVRayTracingExtensionName);
 
-    vk::PhysicalDevice physical_device = nullptr;
+    vk::PhysicalDevice adapter = nullptr;
 
     auto physical_devices = instance.enumeratePhysicalDevices();
     if (physical_devices.empty()) {
         throw std::runtime_error("Failed to find GPUs with Vulkan support!");
     }
 
+    // Should be in function
     size_t i = 0;
-    for (const auto &device: physical_devices) {
-        auto properties = device.getProperties();
-        auto type = get_physical_device_type_name(properties.deviceType);
-        std::cout << "GPU " << i << ": " << properties.deviceName << " (" << type << ")\n";
+    for (const auto &physical_device: physical_devices) {
+        auto properties = physical_device.getProperties();
+        auto available_device_extension_properties = physical_device.enumerateDeviceExtensionProperties();
 
-        auto available_device_extension_properties = device.enumerateDeviceExtensionProperties();
+        auto type = get_physical_device_type_name(properties.deviceType);
+        spdlog::info("GPU {}: {} ({})", i, properties.deviceName.data(), type);
 
         std::set<std::string> available_device_extension_names;
         for (auto extension_property: available_device_extension_properties) {
@@ -158,37 +180,68 @@ int main() {
         }
 
         std::set<std::string> missing_device_extension_names;
-        for (auto extension_name: required_device_extension_names) {
+        for (const auto &extension_name: required_device_extension_names) {
             if (!available_device_extension_names.contains(extension_name)) {
                 missing_device_extension_names.insert(extension_name);
             }
         }
 
         if (missing_device_extension_names.empty()) {
-            std::cout << "    All required extensions supported. Using this device";
-            physical_device = device;
+            spdlog::info("    All required extensions supported. Using this device");
+            adapter = physical_device;
             break;
         } else {
-            std::cout << "    Required device extensions missing: \n";
-            for (auto extension_name: missing_device_extension_names) {
-                std::cout << "        - " << extension_name << "\n";
+            spdlog::info("    Required device extensions missing: ");
+            for (const auto &extension_name: missing_device_extension_names) {
+                spdlog::info("        - {}", extension_name);
             }
         }
-        std::cout << "\n";
 
         ++i;
     }
-    if (physical_device == nullptr) {
-        throw std::runtime_error("Failed to find a physical device with support for all required extensions");
+    if (adapter == nullptr) {
+        spdlog::critical("Failed to find a physical device with support for all required extensions");
     }
+    // ................
 
-    // std::vector<vk::QueueFamilyProperties> queue_family_properties = physical_device.getQueueFamilyProperties();
+    auto queue_family_properties = adapter.getQueueFamilyProperties();
+
+    size_t queue_family_index = 0;
+
+    i = 0;
+    for (auto queue_family_property: queue_family_properties) {
+        std::string flag_names;
+
+        auto flags = queue_family_property.queueFlags;
+        if (flags & vk::QueueFlagBits::eGraphics) flag_names += "Graphics | ";
+        if (flags & vk::QueueFlagBits::eCompute) flag_names += "Compute | ";
+        if (flags & vk::QueueFlagBits::eTransfer) flag_names += "Transfer | ";
+        if (flags & vk::QueueFlagBits::eSparseBinding) flag_names += "SparseBinding | ";
+        if (flags & vk::QueueFlagBits::eProtected) flag_names += "Protected | ";
+        if (flags & vk::QueueFlagBits::eVideoDecodeKHR) flag_names += "VideoDecodeKHR | ";
+        if (flags & vk::QueueFlagBits::eVideoEncodeKHR) flag_names += "VideoEncodeKHR | ";
+        if (flags & vk::QueueFlagBits::eOpticalFlowNV) flag_names += "OpticalFlowNV | ";
+        if (flags & vk::QueueFlagBits::eDataGraphARM) flag_names += "DataGraphARM | ";
+
+        auto surface_support = false;
+
+        spdlog::info("queue_family_index: {}, queue_count: {}, surface_support: {}, queue_flags: {}", i,
+                     queue_family_property.queueCount, surface_support,
+                     flag_names);
+
+        if (flags & vk::QueueFlagBits::eCompute && !(flags & vk::QueueFlagBits::eGraphics)) {
+            queue_family_index = i;
+        }
+
+        ++i;
+    }
 
     // vk::DeviceQueueCreateInfo device_queue_create_info { .queueFamilyIndex = graphicsIndex };
 
     // while (!glfwWindowShouldClose(window)) {
     //     glfwPollEvents();
     // }
+
     glfwDestroyWindow(window);
     glfwTerminate();
 
