506c442f891782a600215f25db6b7c2c1aa72130
gavrix32 <superdimoxa228@gmail.com>
2026-05-12 07:34:07 +0300

    Try to fix the Windows workflow (swapchain)

--- src/vulkan/swapchain.cpp
+++ src/vulkan/swapchain.cpp
@@ -1,10 +1,10 @@
-#include <GLFW/glfw3.h>
-
+#include "swapchain.h"
 #include "adapter.h"
 #include "device.h"
 #include "image.h"
 #include "utils.h"
-#include "swapchain.h"
+
+#include <GLFW/glfw3.h>
 
 vk::Extent2D choose_extent(GLFWwindow* window, const vk::SurfaceCapabilitiesKHR& capabilities) {
     if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
